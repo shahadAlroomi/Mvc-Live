0ab4a9a14e2676840120359ab8df5312cc2d8157
shahadAlroomi <shahad.ro@icloud.com>
2026-02-03 14:00:48 +0100

    update HomeController

--- Controlles/HomeController.cs
+++ Controlles/HomeController.cs
@@ -5,10 +5,13 @@ public class HomeController :Controller // A base class for an MVC controller wi
 {
     public IActionResult Index()
     {
-        return View(); // Returns: The created ViewResult object for the response.
+        List<Dog> dogs =[new("Fido","Labrador",10),new ("bella ","colle",15)];
+        return View(dogs); // Returns: The created ViewResult object for the response.
     }
     public IActionResult Dogs()
     {
         return View();
     }
 }
+
+record Dog (string Name , string Breed ,int Age);

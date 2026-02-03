e3208012d786d8ee6b0401e3464599f058753020
shahadAlroomi <shahad.ro@icloud.com>
2026-02-03 11:23:36 +0100

    1 steg

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+﻿// Mvc : 3)model 2)view 1)controller 
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+
+app.MapDefaultControllerRoute();
+
+
+
+
+
+
+
+
+
+
+
+
+app.Run();
 

16868528269be1c7c9b0e40c722264bfbffff388
shahadAlroomi <shahad.ro@icloud.com>
2026-02-04 09:09:45 +0100

    Add cat and update dog

--- Controlles/HomeController.cs
+++ Controlles/HomeController.cs
@@ -5,13 +5,20 @@ public class HomeController :Controller // A base class for an MVC controller wi
 {
     public IActionResult Index()
     {
-        List<Dog> dogs =[new("Fido","Labrador",10),new ("bella ","colle",15)];
-        return View(dogs); // Returns: The created ViewResult object for the response.
+        return View(); // Returns: The created ViewResult object for the response.
     }
     public IActionResult Dogs()
     {
-        return View();
+        List<Dog> dogs =[new("Fido","Labrador",10),new ("Bella ","Colle",12),new("max","bulldog",5),new("rocky","poodle",3)];
+
+        return View(dogs);
+    }
+    public IActionResult Cats()
+    {
+        List<Cat> cats = [new("Whiskers","Persian",2),new("Luna","Maine Coon",4),new("Simba","Bengal",1)];
+        return View(cats);
     }
 }
 
 record Dog (string Name , string Breed ,int Age);
+record Cat (string Name , string Breed ,int Age);
